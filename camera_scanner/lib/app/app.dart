89ed2bf3af7b138ca576666539dc061b56ab2823
clay <hansolmin3@gmail.com>
2021-11-08 01:42:24 +0900

    fix : delete showing debug mode label

--- camera_scanner/lib/app/app.dart
+++ camera_scanner/lib/app/app.dart
@@ -14,6 +14,7 @@ class _MlCameraApp extends State<MlCameraApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: CameraPage(),
     );
   }
